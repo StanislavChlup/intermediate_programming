2c488d9df367dcadf0f9754157400944104e6369
Stanislav Chlup <standachlupcz@gmail.com>
2023-10-31 18:17:42 +0100

    methods

--- methods-excercise.cpp
+++ methods-excercise.cpp
@@ -14,20 +14,36 @@ struct Rectangle {
     Point topLeft;
     Point bottomRight;
 
-    int width() const;
-    int height() const;
-    int area() const;
-    bool contains(const Point& p) const;
 
-    void move(int dx, int dy);
+    int width() const {
+        return std::abs(topLeft.x - bottomRight.x);
+    }
+    int height() const {
+        return std::abs(topLeft.y - bottomRight.y);
+    }
+    int area() const {
+        return width() * height();
+    }
+    bool contains(const Point& p) const {
+        return p.x >= topLeft.x && p.x <= bottomRight.x && p.y >= topLeft.y && p.y <= bottomRight.y;
+    }
+
+    void move(int dx, int dy) {
+        topLeft.x += dx;
+        topLeft.y += dy;
+        bottomRight.x += dx;
+        bottomRight.y += dy;
+    }
 };
 
 int main() {
     Rectangle r = {{0, 0}, {10, 10}};
     const Rectangle& cr = r;
+    
     assert(cr.width() == 10);
     assert(cr.height() == 10);
     assert(cr.area() == 100);
+    
     assert(cr.contains({5, 5}));
     assert(!cr.contains({-1, -1}));
     assert(!cr.contains({11, 11}));
@@ -35,7 +51,6 @@ int main() {
     r.move(1, 1);
     assert(cr.contains({5, 5}));
     assert(!cr.contains({-1, -1}));
-    assert(!cr.contains({11, 11}));
-
+    assert(cr.contains({11, 11}));
     std::cout << "All tests passed!\n";
 }
